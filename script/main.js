ac76d284c36b0179f59a3bc024177e24fb713395
AlessandroSilvaS <alessandrosoares1409@gmail.com>
2024-12-10 08:50:08 -0300

    dewdewf

--- script/main.js
+++ script/main.js
@@ -1,11 +1,19 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const clicked = () => {
+const fizzBuzz_1 = __importDefault(require("./fizzBuzz"));
+function clicked() {
     let inputArray = document.getElementById('input-array');
     if (inputArray.value !== '') {
         let valueInput = inputArray.value;
+        const valueInputString = valueInput.split(/[\s,]+/);
         try {
-            const valueInputNumber = parseInt(valueInput);
+            const valueInputNumber = valueInputString.map((value) => {
+                return parseInt(value);
+            });
+            console.log((0, fizzBuzz_1.default)(valueInputNumber));
         }
         catch (error) {
             console.log(error);
@@ -14,4 +22,4 @@ const clicked = () => {
     else {
         window.alert("Por favor, preencha o campo corretamente...");
     }
-};
+}
--- types/main.ts
+++ types/main.ts
@@ -1,12 +1,18 @@
 import fizzBuzz from "./fizzBuzz";
 
-const clicked = () => {
+function clicked(){
     let inputArray = document.getElementById('input-array') as HTMLInputElement
 
     if(inputArray.value !== ''){
         let valueInput:string = inputArray.value
+        const valueInputString:Array<string> = valueInput.split(/[\s,]+/)
+
         try {
-            const valueInputNumber:number = parseInt(valueInput)
+            const valueInputNumber:Array<number> = valueInputString.map((value) => {
+                return parseInt(value)
+            })
+
+            console.log(fizzBuzz(valueInputNumber))
         } catch (error) {
             console.log(error)
         }
